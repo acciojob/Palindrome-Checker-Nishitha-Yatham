6828948511a90bc7fc1c9d4d10e061b0eaed9808
Nishitha-Yatham <120168184+Nishitha-Yatham@users.noreply.github.com>
2023-01-25 22:35:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
 // complete the given function
 
 function palindrome(str){
-     const len=str.length-1;
-	for(let i=0;i<len/2;i++)
-		{
-			let x=str[i];
-			let y=str[len-1];
-			if(x!=y){
-				return false;
-			}
-		}
-	return true;
+     const alphanumericOnly=str.toLowerCase().match(/[a-z0-9]/g);
+	 return alphanumericOnly.join('') === alphanumericOnly.reverse().join('');
 }
 console.log(palindrome("racecar"));
 module.exports = palindrome
