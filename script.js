9d5f95cbcf3713fa8a97d9601b17eff45d492574
Nishitha-Yatham <120168184+Nishitha-Yatham@users.noreply.github.com>
2023-01-25 21:27:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // complete the given function
 
 function palindrome(str){
-
+     const len=str.length;
+	for(let i=0;i<len/2;i++)
+		{
+			if(str[i]!==str[len-1-i]){
+				return false;
+			}
+			else{
+				return true;
+			}
+		}
 }
 module.exports = palindrome
